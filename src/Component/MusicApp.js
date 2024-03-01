88fca64778c903c1288fb5a4a86e7fd527fd278f
akshaynalkol <akshaynalkol2001@gmail.com>
2024-03-01 16:31:16 +0530

    Commit

--- src/Component/MusicApp.js
+++ src/Component/MusicApp.js
@@ -19,9 +19,9 @@ export default function MusicApp() {
         setIsLoading(false);
     }
 
-    // useEffect(() => {
-    //     getTracks();
-    // }, []);
+    useEffect(() => {
+        getTracks();
+    }, []);
 
 
     return (
